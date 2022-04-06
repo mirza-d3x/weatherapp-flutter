bed18d22afdb8fecb676e34b605ecef9eb702d7d
Mohammed Mirsa <mohammedmirsa010@gmail.com>
2022-04-06 15:06:46 +0530

    Date Time, Location and Tempreature added

--- lib/MyHomePag.dart
+++ lib/MyHomePag.dart
@@ -1,24 +1,100 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
+import 'package:weatherapp/DataModel.dart';
+import 'package:weatherapp/fetchweatherapi.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  DataModel? weatherData;
+  String timeNow = DateFormat("hh:mm: a").format(DateTime.now());
+
+  @override
+  void initState() {
+    fetchWeather("London");
+    // TODO: implement initState
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        decoration:  BoxDecoration(
-
-          image: DecorationImage(
-            image: AssetImage("assets/images/nightmoon.jpg"),
-          fit: BoxFit.cover),
+    return SafeArea(
+      child: Scaffold(
+        body: FutureBuilder(
+          future: fetchWeather("London"),
+          builder: (BuildContext context, AsyncSnapshot<DataModel> snapshot) {
+            if (snapshot.hasData) {
+              return Container(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("assets/images/nightmoon.jpg"),
+                      fit: BoxFit.cover),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          snapshot.data!.current!.tempC.toString(),
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.dosis(
+                            color: Colors.white,
+                            fontSize: 80,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 60,left: 10),
+                          child: Text(
+                            "o",
+                            style: GoogleFonts.dosis(
+                              color: Colors.white,
+                              fontSize: 50,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Row(
+                          children: [
+                            Text(
+                              timeNow,
+                              textAlign: TextAlign.center,
+                              style: GoogleFonts.dosis(
+                                color: Colors.white,
+                                fontSize: 80,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Text(
+                          snapshot.data!.location!.name.toString(),
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.dosis(
+                            color: Colors.white,
+                            fontSize: 80,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              );
+            }
+            return Center(child: CircularProgressIndicator());
+          },
         ),
-        child: Text("11",style: GoogleFonts.rubikMonoOne(
-          color: Colors.yellow,
-        ),),
       ),
     );
   }
---
+++ lib/fetchweatherapi.dart
@@ -0,0 +1,16 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:weatherapp/DataModel.dart';
+
+Future<DataModel> fetchWeather(String CityName) async {
+  final result = await http.get(
+      Uri.parse('https://weatherapi-com.p.rapidapi.com/current.json?q='+CityName),
+      headers: {
+        'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com',
+        'X-RapidAPI-Key': 'e5eaa77c92msh9b400b217852dd9p1db858jsnf74960762d37'
+      });
+  print(result.statusCode);
+  print("https://weatherapi-com.p.rapidapi.com/current.json?q="+ CityName);
+  print(result.body);
+  return DataModel.fromJson(json.decode(result.body));
+}
